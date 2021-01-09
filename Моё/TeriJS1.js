7af417f7f5c01d6d3ff1a2d5345d065c592024f7
Teri-IM <63192118+Teri-IM@users.noreply.github.com>
2021-01-09 20:16:57 +0300

    eeee

--- Моё/TeriJS1.js
+++ Моё/TeriJS1.js
@@ -10,6 +10,7 @@ function consoleText(words, id, colors) {
  var target = document.getElementById(id)
  target.setAttribute('style', 'color:' + colors[0])
  window.setInterval(function() {
+
    if (letterCount === 0 && waiting === false) {
      waiting = true;
      target.innerHTML = words[0].substring(0, letterCount)
